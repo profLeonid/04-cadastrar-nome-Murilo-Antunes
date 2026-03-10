431a56043bf5af94e70473c0501abc4728284aba
MURILO ANTUNES CAMPOS DA ROCHA <25203706@cfp127aluno.local>
2026-03-10 11:05:36 -0300

    finalizando logica principal do programa

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 'use strict'
 const btNome = document.getElementById('nome')
 const saida = document.getElementById('saida')
-const linha = document.getElementById('linha')
-const p = document.createElement('p')
+
 const salvarNome =  function(){
+    const p = document.createElement('p')
     p.textContent = btNome.value
 
-    saida.appendChild(linha)
-    linha.appendChild(p)
+    saida.appendChild(p)
 }
